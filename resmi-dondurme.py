db092d7330cc1820e1b75c19662fb7a167b9171b
Ahmet Karakaya <karakayahmet00@gmail.com>
2022-10-30 14:25:06 +0300

    resim döndürme py oluşturuldu.

--- resmi-dondurme.py
+++ resmi-dondurme.py
@@ -4,12 +4,12 @@ import numpy as np
 img = cv2.imread("heli.jpg",0)
 row,col = img.shape
 
-m = np.float32([[1,0,10],[0,1,10]])
+m = cv2.getRotationMatrix2D((col/2,row/2), 90, 1)
 
 dst = cv2.warpAffine(img,m,(col,row))
 
 cv2.imshow("original",img)
-cv2.imshow("yeni",dst)
+cv2.imshow("dondurme",dst)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
